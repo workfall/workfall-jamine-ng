7eed7777b4b4a1d28c211626f5287cc8db09d111
Elon Aseneka <elonsantos63@gmail.com>
2022-12-02 22:40:49 +0300

    move components to shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './components/nav-bar/nav-bar.component';
-import { ProductComponent } from './components/product/product.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from './modules/shared/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavBarComponent,
-    ProductComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavBarComponent } from 'src/app/components/nav-bar/nav-bar.component';
+import { ProductComponent } from 'src/app/components/product/product.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NavBarComponent,
+    ProductComponent
+  ],
   imports: [
     CommonModule
   ]
